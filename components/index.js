2579a33714e7cd8eb93e71bee7cd77581cb5a428
zeynepbozkır <bozkrzeynep@gmail.com>
2022-10-06 11:11:36 +0300

    make opration

--- components/index.js
+++ components/index.js
@@ -1,31 +1,84 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import {useState} from "react";
+import {useState, useRef, useEffect} from "react";
+
 
  function Calculator() {
-    const numArr=[0,1,2,3,4,5,6,7,8,9]
-     const [val,setVal]  =useState([])
+     const ref = useRef();
 
-     const onClickButon=(b)=>{
-        setVal(val+b)
-         console.log(val,"bbb")
+
+
+
+     // const prevCount=usePrevious()
+
+
+     const numArr=[0,1,2,3,4,5,6,7,8,9]
+     const operationArr=["+","-","*","/","="]
+     const [val,setVal]  =useState( 0)
+     const [result,setResult]  =useState(0)
+     const [opr,setOpr]  =useState("")
+     const [son,setSon]  =useState(0)
+
+
+     useEffect(() => {
+         ref.current = val;
+     },[val]);
+
+     const onClickNum=(number)=>{
+        if( opr !=="=" || !val){
+            setVal((val+number).toString())
+        }
      }
+     const onClickOpr = (operation) => {
 
 
-    return (
+         if (operation !== "=") {
+             setVal(0)
+             setOpr(operation)
+             if(result){
+                 setResult(result)
+             }
+             else if(!result){
+                 setResult(val)
+             }
+         }
+         if (operation === "=") {
+
+             setResult(eval(`${result}
+             ${opr}
+             ${val} `))
+
+         }
+
+
+
+     }
+     return (
         <div className={styles.container}>
-            {numArr.map((a,index)=>
-
-                <button value={a}
-                        key={index}
+          {numArr.map((num,i)=>
+                <button value={num}
+                        key={i}
                         onClick={()=> {
-                            onClickButon(a)
+                            onClickNum(num)
                         }}
-                >{a}</button>
+                >{num}</button>
             )
             }
 
+
+    {operationArr.map((opr,i)=>
+    <button value={opr}
+            key={i}
+        onClick={()=> {
+            onClickOpr(opr)
+        }}
+    >{opr}   </button>
+
+)}
+
+           {/*{val}*/}
+            {result}
 
 
         </div>
