edc8d4730a61d234ed19d6430fb9712ac8730c69
zeynepbozkır <bozkrzeynep@gmail.com>
2022-10-15 17:40:42 +0300

    add

--- components/index.js
+++ components/index.js
@@ -1,101 +1,157 @@
-import {useState, useRef, useEffect} from "react";
-import styles from  "./component.module.css"
+import { useState, useRef, useEffect } from "react";
+import styles from "./component.module.css";
 
 export default function Calculator() {
+  const valueARR = [
+    {
+      value: 9,
+      type: "number",
+    },
+    {
+      value: 8,
+      type: "number",
+    },
+    {
+      value: 7,
+      type: "number",
+    },
+    {
+      value: 6,
+      type: "number",
+    },
+    {
+      value: 5,
+      type: "number",
+    },
+    {
+      value: 4,
+      type: "number",
+    },
+    {
+      value: 3,
+      type: "number",
+    },
+    {
+      value: 2,
+      type: "number",
+    },
+    {
+      value: 1,
+      type: "number",
+    },
+    {
+      value: 0,
+      type: "number",
+    },
+    {
+      value: ".",
+      type: "number",
+    },
+    {
+      value: "+",
+      type: "operation",
+    },
+    {
+      value: "-",
+      type: "operation",
+    },
+    {
+      value: "*",
+      type: "operation",
+    },
+    {
+      value: "/",
+      type: "operation",
+    },
+    {
+      value: "=",
+      type: "operation",
+    },
+    {
+      value: "C",
+      type: "operation",
+    },
+    {
+      value: "+/-",
+      type: "operation",
+    },
+  ];
+  const [val, setVal] = useState(0);
+  const [result, setResult] = useState(0);
+  const [opr, setOpr] = useState("");
 
-     const numArr=[9,8,7,6,5,4,3,2,1,0]
-     const operationArr=["+","-","*","/","=","C","+/-",","]
-     const [val,setVal]  =useState( 0)
-     const [result,setResult]  =useState(0)
-     const [opr,setOpr]  =useState("")
-     const [click,setClick]  =useState(false)
-     const [num, setNum] = useState(0);
+  const onClickNum = (number) => {
+    if (val !== "0.") {
+      setVal((val + number).toString());
+      // setVal((prevState) => (prevState + number).toString());
+      console.log(typeof number, "nnn");
+    } else {
+      if (number !== ".") {
+        setVal((val + number).toString());
+      }
+    }
+  };
+  const onClickOpr = (operation) => {
+    if (
+      operation !== "=" &&
+      operation !== "C" &&
+      operation !== "+/-" &&
+      operation !== ","
+    ) {
+      setOpr(operation);
+      setVal(0);
 
-
-
-     const onClickNum=(number)=>{
-
-         setVal((val+number).toString())
-        //  else{
-        //     setVal(0)
-        // }
-     }
-     const onClickOpr = (operation) => {
-
-         if (operation !== "=" && operation !== "C" && operation !==  "+/-"  && operation !==  ",") {
-             setOpr(operation)
-             setVal(0)
-             if(result){
-                 setResult(result)
-             }
-             else if(!result){
-                 setResult(val)
-             }
-
-         }
-
-         else  if( operation === "+/-"){
-             console.log(opr,"opr +-")
-             setVal((val * -1).toString())
-             setResult(val)
-         }
-
-         else if(operation === "C"){
-             setVal(0)
-             setResult(0)
-         }
-         else if(operation === ","){
-              setVal(val + ",")
-              setResult(val)
-          }
-
-         else  if (operation === "=") {
-             if(!val){
-                 setResult((prevNum) => prevNum)
-             }
-             else if(val) {
-                 setResult(eval(`${result}
+      if (result) {
+        setResult(result);
+      } else {
+        setResult(val);
+      }
+    } else if (operation === "+/-") {
+      console.log(opr, "opr +-");
+      setVal((val * -1).toString());
+      setResult(val);
+    } else if (operation === "C") {
+      setVal(0);
+      setResult(0);
+    } else if (operation === "=") {
+      if (!val) {
+        setVal(result);
+        setResult(
+          eval(`${result}
              ${opr}
-             ${val} `))
+             ${result}`)
+        );
+      } else {
+        setResult(
+          eval(`${result}
+             ${opr}
+             ${val}`)
+        );
+      }
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <div>
+        val: {val}/{result}
+        <br />
+      </div>
 
-             }
-
-
-         }
-
-     }
-     return (
-        <div className={styles.container}>
-          {numArr.map((num,i)=>
-                <button  className={styles.button} value={num}
-                        key={i}
-                        onClick={()=> {
-                            onClickNum(num)
-                        }}
-                >{num}  </button>
-            )
-            }
-
-
-    {operationArr.map((opr,i)=>
-    <button value={opr}
+      <div className={styles.button}>
+        {valueARR.map((num, i) => (
+          <button
+            value={num.value}
             key={i}
-        onClick={()=> {
-            onClickOpr(opr)
-        }}
-    >{opr}   </button>
-
-)}
-
-          val: {val}/
-            {result}
-
-
-
-            <button onClick={() => setNum((prevNum) => prevNum+1 )}>
-                Increment
-            </button>
-            {num}
-        </div>
-    )
+            onClick={() => {
+              num.type === "number"
+                ? onClickNum(num.value)
+                : onClickOpr(num.value);
+            }}
+          >
+            {num.value}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
